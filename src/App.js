6e169f20534a2756acdbd32a7cf6cdd040247d6b
marlbones <mdimaanods@gmail.com>
2017-05-30 15:33:39 +1000

    Minor styling and react-materialize framework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Card, Row, Col } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>On Call Assistant</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
