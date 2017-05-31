9726d1c241d04e2e6c1e091fa921e10f480ae8e6
marlbones <mdimaanods@gmail.com>
2017-05-31 12:02:54 +1000

    Yarn added axios. Added Http request function to grab client api. Propped it to Clients components

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
 import './Clients.css';
+
+class Clients extends Component {
+  render() {
+    return (
+      <div>
+        <h5>Client Database</h5>
+        {console.log(this.props.clients)}
+      </div>
+    )
+  }
+}
+
+export default Clients;
