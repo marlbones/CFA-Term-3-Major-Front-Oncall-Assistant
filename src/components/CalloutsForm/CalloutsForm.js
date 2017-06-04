56565a27397a1a7ede036519e31106bf104ebe24
marlbones <mdimaanods@gmail.com>
2017-06-04 14:24:35 +1000

    Selector for months made using function and state in CalloutsForm

--- src/components/CalloutsForm/CalloutsForm.js
+++ src/components/CalloutsForm/CalloutsForm.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './CalloutsForm.css';
+import { Input } from 'react-materialize';
 
 class CalloutsForm extends Component {
-  
+  constructor(props) {
+  super(props);
+  this.state = {
+    monthSelected: 'January'
+  };
+  this.handleSelected = this.handleSelected.bind(this);
+}
+
+  handleSelected(e) {
+  this.setState({ monthSelected: e.target.value });
+}
+
   newCallout() {
     const URL = 'https://oncallback.herokuapp.com/callouts';
-    axios.post(URL + '?month=' + this.monthInput.value)
+    axios.post(URL + '?month=' + this.state.monthSelected)
       .then((response) => {
         console.log(response);
         this.monthInput = '';
@@ -19,7 +31,20 @@ class CalloutsForm extends Component {
   render() {
     return (
       <div>
-        <input type="text" ref={(input) => { this.monthInput = input; }} placeholder="Month" />
+        <Input type="select" onChange={this.handleSelected} placeholder="Month">
+          <option value='January'>January</option>
+          <option value='February'>February</option>
+          <option value='March'>March</option>
+          <option value='April'>April</option>
+          <option value='May'>May</option>
+          <option value='June'>June</option>
+          <option value='July'>July</option>
+          <option value='August'>August</option>
+          <option value='September'>September</option>
+          <option value='October'>October</option>
+          <option value='November'>November</option>
+          <option value='December'>December</option>
+        </Input>
         <button onClick={() => this.newCallout()}>Submit</button>
       </div>
     )
