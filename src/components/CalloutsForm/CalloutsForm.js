0f86e7776f2d3a0680a6afdee1bbe8dfc37f8965
marlbones <mdimaanods@gmail.com>
2017-06-04 12:13:12 +1000

    Callouts creation working in CalloutsForm. Month input only so far

--- src/components/CalloutsForm/CalloutsForm.js
+++ src/components/CalloutsForm/CalloutsForm.js
@@ -3,11 +3,24 @@ import axios from 'axios';
 import './CalloutsForm.css';
 
 class CalloutsForm extends Component {
+  
+  newCallout() {
+    const URL = 'https://oncallback.herokuapp.com/callouts';
+    axios.post(URL + '?month=' + this.monthInput.value)
+      .then((response) => {
+        console.log(response);
+        this.monthInput = '';
+      })
+      .catch(function (err) {
+        console.log(err)
+      });
+  };
+
   render() {
     return (
       <div>
-        <input />
-        <button>Add</button>
+        <input type="text" ref={(input) => { this.monthInput = input; }} placeholder="Month" />
+        <button onClick={() => this.newCallout()}>Submit</button>
       </div>
     )
   }
